aa7cc49f11c738b6b18f722e4011dde89a3fb826
Anthony Allison <139900552+AnthonyAll181@users.noreply.github.com>
2023-10-06 23:56:21 +0200

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,66 @@
+    main()
+        print(f"Random Number: {item}")
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+
+def main():
+    for item in data:
+
+    data = generate_random_data()
+
+
+if __name__ == "__main__":
+def generate_random_data():def main():
+import random
+    for item in data:
+
+    data = generate_random_data()
+
+
+
+    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")
+def generate_random_data():
+    return data
+if __name__ == "__main__":
+    main()
+if __name__ == "__main__":
+        print(f"Random Number: {item}")
+    data = generate_random_data()
+
+    for item in data:
+def generate_random_data():
+
+
+import random
+    return data
+def main():
+    main()
+    data = [random.randint(1, 100) for _ in range(10)]def main():
+    data = generate_random_data()
+
+import random
+def generate_random_data():
+    for item in data:
+    main()
+
+
+if __name__ == "__main__":
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+        print(f"Random Number: {item}")def main():
+        print(f"Random Number: {item}")
+
+    for item in data:
+    return data
+
+    data = [random.randint(1, 100) for _ in range(10)]
+if __name__ == "__main__":
+
+    main()
+
+def generate_random_data():
+import random
+    data = generate_random_data()
