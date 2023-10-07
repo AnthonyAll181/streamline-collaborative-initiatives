d4471c5c87413911e72816ecc825cc2bd13f3a42
Anthony Allison <139900552+AnthonyAll181@users.noreply.github.com>
2023-10-07 08:57:05 +0200

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,30 @@
+    data = [random.randint(1, 100) for _ in range(10)]
+
+
+def main():
+    for item in data:
+        print(f"Random Number: {item}")
+
+if __name__ == "__main__":
+    data = generate_random_data()
+    return data
+import random
+    main()
+
+def generate_random_data():function generateRandomData() {
+}
+        console.log("Random Number: " + item);
+    data.forEach(item => {
+    const data = [];
+    });
+    for (let i = 0; i < 10; i++) {
+
+    return data;
+
+    }
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+    const data = generateRandomData();
+}
+function main() {
+main();
