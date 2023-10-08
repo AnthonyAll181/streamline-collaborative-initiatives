e4cca259564ab8073c0e2faed3f83932b8061962
Anthony Allison <139900552+AnthonyAll181@users.noreply.github.com>
2023-10-08 20:59:53 +0200

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,25 +1,29 @@
-
-        for (int i = 0; i < 10; i++) {
-    }
-            System.out.println("Random Number: " + item);
-        for (int item : data) {
-        }
-        int[] data = new int[10];
-        }
+main();
+    const data = [];
+function main() {
 }
 
-public class RandomDataGenerator {
-    public static void main(String[] args) {
-            data[i] = (int) (Math.random() * 100) + 1;
-    public static void main(String[] args) {
+    });
+    for (let i = 0; i < 10; i++) {
 }
-        int[] data = new int[10];
-        }
-    }
-            System.out.println("Random Number: " + item);
-public class RandomDataGenerator {
-        }
-        for (int item : data) {
-        for (int i = 0; i < 10; i++) {
+    const data = generateRandomData();
+function generateRandomData() {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    return data;
+    data.forEach(item => {
 
+        console.log("Random Number: " + item);
+
+    }
+        for (int i = 0; i < 10; i++) {
+        }
+        int[] data = new int[10];
+}
+        }
+public class RandomDataGenerator {
+    public static void main(String[] args) {
+            System.out.println("Random Number: " + item);
+
+    }
             data[i] = (int) (Math.random() * 100) + 1;
+        for (int item : data) {
