14902b2e2721b7c2bc0b6967b4e5b66b337e5d33
Anthony Allison <139900552+AnthonyAll181@users.noreply.github.com>
2023-10-09 15:01:18 +0200

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,29 +1,52 @@
-main();
-    const data = [];
-function main() {
-}
+    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")
+def main():
 
-    });
-    for (let i = 0; i < 10; i++) {
-}
-    const data = generateRandomData();
-function generateRandomData() {
-        data.push(Math.floor(Math.random() * 100) + 1);
-    return data;
-    data.forEach(item => {
 
-        console.log("Random Number: " + item);
+def generate_random_data():
 
-    }
+if __name__ == "__main__":
+    main()
+    for item in data:
+    data = generate_random_data()
+    return data
+import random
+    return data
+    data = generate_random_data()
+def main():
+    main()
+    data = [random.randint(1, 100) for _ in range(10)]
+import random
+
+if __name__ == "__main__":
+def generate_random_data():
+    for item in data:
+        print(f"Random Number: {item}")
+
+
+    data = generate_random_data()
+
+    main()
+if __name__ == "__main__":
+        print(f"Random Number: {item}")
+def main():
+
+def generate_random_data():
+    for item in data:
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+
         for (int i = 0; i < 10; i++) {
-        }
-        int[] data = new int[10];
+            System.out.println("Random Number: " + item);
 }
+
         }
+        }
+    }
 public class RandomDataGenerator {
     public static void main(String[] args) {
-            System.out.println("Random Number: " + item);
+        int[] data = new int[10];
 
-    }
-            data[i] = (int) (Math.random() * 100) + 1;
         for (int item : data) {
+            data[i] = (int) (Math.random() * 100) + 1;
