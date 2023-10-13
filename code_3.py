f86584e0f26a9785784aa3ebf733018cac25efbf
Anthony Allison <139900552+AnthonyAll181@users.noreply.github.com>
2023-10-13 18:07:53 +0200

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,13 +1,52 @@
-import random
-        print(f"Random Number: {item}")
-    main()
-
-
-    for item in data:
-    data = generate_random_data()
-
     data = [random.randint(1, 100) for _ in range(10)]
+
 def generate_random_data():
+    data = generate_random_data()
+    for item in data:
+        print(f"Random Number: {item}")
+import random
+
+
 def main():
     return data
 if __name__ == "__main__":
+
+    main()    for item in data:
+if __name__ == "__main__":
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()
+        print(f"Random Number: {item}")
+    main()
+def generate_random_data():
+def main():
+
+
+import random
+
+    return data
+def main():
+import random
+def generate_random_data():
+    for item in data:
+
+        print(f"Random Number: {item}")
+    data = generate_random_data()
+
+
+    return data
+    main()
+    data = [random.randint(1, 100) for _ in range(10)]
+
+if __name__ == "__main__":}
+        for (int i = 0; i < 10; i++) {
+
+
+    public static void main(String[] args) {
+        for (int item : data) {
+    }
+            System.out.println("Random Number: " + item);
+            data[i] = (int) (Math.random() * 100) + 1;
+        int[] data = new int[10];
+        }
+public class RandomDataGenerator {
+        }
